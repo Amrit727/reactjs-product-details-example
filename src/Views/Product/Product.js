54079885784472d34957de17306493e82f21fe35
cagdas.yigit <cagdas.yigit@pia-team.com>
2018-11-30 18:55:27 +0300

    initial

---
+++ src/Views/Product/Product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Product.css';
+
+const Product = ( props ) => {
+    return (
+        <div className="Product">
+            Product
+        </div>
+    )
+};
+
+export default Product;
